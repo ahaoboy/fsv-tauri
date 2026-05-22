541ddf01b7796a89739f88ba5676541434fc1340
ahaoboy <504595380@qq.com>
2026-05-22 12:19:20 +0800

    apk

---
+++ src-tauri/gen/android/app/src/main/java/com/ahaoboy/fsv_tauri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ahaoboy.fsv_tauri
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
